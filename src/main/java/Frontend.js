d370322a17e023d87bdd22c8432bdb8f01fb5405
gregor <gregor.christiner@gmail.com>
2023-08-08 10:46:53 +0200

    Javascirpt und Index.html zwischenschritte.

--- src/main/java/Frontend.js
+++ src/main/java/Frontend.js
@@ -2,7 +2,36 @@ fetch('Output.json')
             .then(response => response.json())
             .then(data => {
 
-                var newText = JSON.stringify(data,null,2);
+                var newText = JSON.stringify(data);
                 var oldText = document.getElementById('Test');
                 oldText.innerHTML = newText;
+
+                var arrayString = JSON.stringify(data);
+                var arrayTest = JSON.parse(arrayString);
+                var Array = [];
+
+                Array = arrayTest.cards;
+
+                var div_header = document.createElement('div');
+                div_header.setAttribute('class','header');
+
+                var elem2 = document.createElement('label');
+                elem2.innerHTML = "something";
+
+                div_header.appendChild(elem2);
+                document.body.appendChild(div_header);
+
+                console.log(CleanJSON(arrayString));
             });
+
+function CleanJSON(Input){
+
+var FirstLetter = Input.slice(1);
+var LastLetter = FirstLetter.slice(0, this.length - 1);
+
+var Output = [];
+
+Output = LastLetter.split(",")
+
+return(Output);
+}
