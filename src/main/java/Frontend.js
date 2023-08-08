10b5266ff275d77a1c856f227ec7cb5e04e6f258
gregor <gregor.christiner@gmail.com>
2023-08-08 15:22:11 +0200

    Javascirpt und Index.html zwischenschritte.

--- src/main/java/Frontend.js
+++ src/main/java/Frontend.js
@@ -7,8 +7,6 @@ fetch('Output.json')
                 var arrayString = JSON.stringify(data);
                 var CleanString = CleanJSON(arrayString);
 
-                console.log(o);
-                console.log(k);
                 CreateDiv(CleanString);
 
 
@@ -16,8 +14,6 @@ fetch('Output.json')
                     CreateDiv(CleanString);
                     o++;
                 }
-
-
 
             });
 
@@ -30,30 +26,69 @@ var l = 0;
 function CleanJSON(Input){
 
 var FirstLetter = Input.slice(1);
-var LastLetter = FirstLetter.slice(0, this.length - 1);
+var LastLetter = FirstLetter.slice(0, this.length - 2);
+
+var MultipleJSONStrings = LastLetter.split("}");
+var NewMultipleJSONStringsLoop=[];
+var NewMultipleJSONStrings=[];
+var z = 0;
+
+MultipleJSONStrings.forEach(element => {
+    var u = 1;
+
+    while(MultipleJSONStrings[0].charAt(u) != "{"){
+        u++;
+    }
+    u++;
+
+    NewMultipleJSONStringsLoop[z]=MultipleJSONStrings[z].slice(u);
+    NewMultipleJSONStrings[z]=MultipleJSONStrings[z].slice(u);
+
+    z++;
+});
 
 var SingleJSONStrings = [];
+var RAM = [];
+    var t = 0;
+    var r = 0;
 
-SingleJSONStrings = LastLetter.split(",");
+NewMultipleJSONStringsLoop.forEach(element => {
+
+    RAM = NewMultipleJSONStrings[t].split(",");
+    SingleJSONStrings[r] = RAM[0]
+    r++;
+    SingleJSONStrings[r] = RAM[1]
+    r++;
+    t++;
+});
 
 var Output = [];
 var i =0;
-
-console.log(SingleJSONStrings);
 
 SingleJSONStrings.forEach(element => {
 
     k++;
 
     var Data = element.split(":");
+
+    console.log(Data);
+
     Data.forEach(element => {
 
-        if(element.charAt(0) != '"')
+        if(element.charAt(0) != '"' && element.charAt(0) != '{')
         {
             Output[i]=element;
             i++;
+
         }
-        else{
+        if(element.charAt(0) == '{'){
+            var FirstLetterElement = element.slice(2);
+            var LastLetterElement = FirstLetterElement.slice(0, this.length - 1);
+            Output[i]=LastLetterElement;
+            console.log(Output[i]);
+            i++;
+        }
+        if(element.charAt(0) != '{' && element.charAt(0) == '"'){
             var FirstLetterElement = element.slice(1);
             var LastLetterElement = FirstLetterElement.slice(0, this.length - 1);
             Output[i]=LastLetterElement;
@@ -62,7 +97,7 @@ SingleJSONStrings.forEach(element => {
 
     })
 });
-
+console.log(Output);
 return(Output);
 }
 
