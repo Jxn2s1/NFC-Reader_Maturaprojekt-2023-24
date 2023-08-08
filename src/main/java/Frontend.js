3f387f341401f58b09eac470ff35212c00956554
gregor <gregor.christiner@gmail.com>
2023-08-08 12:37:55 +0200

    Javascirpt und Index.html zwischenschritte.

--- src/main/java/Frontend.js
+++ src/main/java/Frontend.js
@@ -1,37 +1,86 @@
+var o = 1;
+
 fetch('Output.json')
             .then(response => response.json())
             .then(data => {
 
-                var newText = JSON.stringify(data);
-                var oldText = document.getElementById('Test');
-                oldText.innerHTML = newText;
-
                 var arrayString = JSON.stringify(data);
-                var arrayTest = JSON.parse(arrayString);
-                var Array = [];
+                var CleanString = CleanJSON(arrayString);
 
-                Array = arrayTest.cards;
+                console.log(o);
+                console.log(k);
+                CreateDiv(CleanString);
 
-                var div_header = document.createElement('div');
-                div_header.setAttribute('class','header');
 
-                var elem2 = document.createElement('label');
-                elem2.innerHTML = "something";
+                while(o < k){
+                    CreateDiv(CleanString);
+                    o++;
+                }
 
-                div_header.appendChild(elem2);
-                document.body.appendChild(div_header);
 
-                console.log(CleanJSON(arrayString));
+
             });
+
+//number of Paragraphs in html
+var k = 0;
+
+//variable for assigning right string to html label/textfield
+var l = 0;
 
 function CleanJSON(Input){
 
 var FirstLetter = Input.slice(1);
 var LastLetter = FirstLetter.slice(0, this.length - 1);
 
-var Output = [];
+var SingleJSONStrings = [];
 
-Output = LastLetter.split(",")
+SingleJSONStrings = LastLetter.split(",");
+
+var Output = [];
+var i =0;
+
+console.log(SingleJSONStrings);
+
+SingleJSONStrings.forEach(element => {
+
+    k++;
+
+    var Data = element.split(":");
+    Data.forEach(element => {
+
+        if(element.charAt(0) != '"')
+        {
+            Output[i]=element;
+            i++;
+        }
+        else{
+            var FirstLetterElement = element.slice(1);
+            var LastLetterElement = FirstLetterElement.slice(0, this.length - 1);
+            Output[i]=LastLetterElement;
+            i++;
+        }
+
+    })
+});
 
 return(Output);
+}
+
+function CreateDiv(Input){
+
+    var div_header = document.createElement('div');
+    div_header.setAttribute('class','header');
+
+    var label = document.createElement('label');
+    label.innerHTML = Input[l];
+    l++;
+
+    var input = document.createElement('input');
+    input.value = Input[l];
+    input.id = o;
+    l++;
+
+    div_header.appendChild(label);
+    div_header.appendChild(input);
+    document.body.appendChild(div_header);
 }
