3db119be54623f2c70d4ff2bfe341bf61449a6a3
Jonas Sakotnik <jonas.sakotnik@rise-world.com>
2023-08-08 09:45:11 +0200

    Works as Webapp -> have to start http-server -p 4040 and open the index.html then run the programm.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+
         //Übergangsmäßige Datei welche den Output der NFC-Chips simuliert
         ExampleNfcData nfcData = new ExampleNfcData(1, "This is the first set of Data", "have fun using it", "just for test purposes");
 
-        //Aufruf der Klasse Output und der Funktion welche die Daten formatiert in die Console ausgibt
         OutputData outputData = new OutputData(nfcData);
+
         outputData.outputToJson();
 
-        try {
-            // Read the modified JSON content and output it
-            String modifiedJsonContent = outputData.readJsonFromFile();
-            System.out.println("Modified JSON content:\n" + modifiedJsonContent);
-
+        //Pausiert das Programm für 15 Sekunden um die Output.json Datei einsehen zu können
+        /*try {
             Thread.sleep(15000);
             System.out.println("After waiting for 15 seconds!");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    */
     }
-
 }
--- src/main/java/OutputData.java
+++ src/main/java/OutputData.java
@@ -1,4 +1,3 @@
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
@@ -17,47 +16,26 @@ public class OutputData {
         numberOfData = data.getNumberOfData();
         outputString = "Data number: " + data.getNumberOfData() + " contains: " + data.getTextValue1() + " , " +
                 data.getTextValue2() + " and " + data.getTextValue3() + ".";
-    }
 
-    //Funktion welche den Output in die Konsole ausgibt
-    public String output() {
-
-        outputString = outputString;
-        return outputString;
+        System.out.println(outputString);
     }
 
     public void outputToJson() {
-        Map<String, Object> jsonMap = new HashMap<>();
+            //Puts the Content of the NFC-Chips in the Output.json
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            File outputFile = new File("src/main/java/Output.json");
+
+            Map<String, Object> jsonMap = new HashMap<>();
         jsonMap.put("outputString", outputString);
         jsonMap.put("numberOfData", numberOfData);
 
-        // Write the JSON object to the Output.json file
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            File outputFile = new File("Output.json");
-            objectMapper.writeValue(outputFile, jsonMap);
-            System.out.println("Data written to Output.json successfully.");
-        } catch (IOException e) {
-            e.printStackTrace();
+            try {
+                objectMapper.writeValue(outputFile, jsonMap);
+                System.out.println("Data written to Output.json successfully.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-    }
-    public String readJsonFromFile() {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            File inputFile = new File("Output.json");
-            Map<String, Object> jsonMap = objectMapper.readValue(inputFile, new TypeReference<Map<String, Object>>() {});
-
-            // Update the class fields based on the content of the JSON file
-            outputString = (String) jsonMap.get("outputString");
-            numberOfData = (int) jsonMap.get("numberOfData");
-
-            // Format and return the JSON content
-            return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonMap);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
 
 }
