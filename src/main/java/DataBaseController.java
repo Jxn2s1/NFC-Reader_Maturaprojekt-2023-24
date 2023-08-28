b6271eecd93f249d2f2dfd4f26007c2aa7146d70
Jonas <74347344+Jxn2s1@users.noreply.github.com>
2023-08-28 14:16:17 +0200

    Moved a few things around for visability

--- src/main/java/DataBaseController.java
+++ src/main/java/DataBaseController.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 //Opens the .exe file which serves as a connection to our Database
 public class DataBaseController {
     public static void openController() {
-        String exePath = "src/main/resources/NFC-Reader/NFC-Reader/bin/Debug/net6.0-windows/NFC-Reader.exe";
+        String exePath = "src/main/dataBaseController/NFC-Reader/NFC-Reader/bin/Debug/net6.0-windows/NFC-Reader.exe";
 
         try {
             ProcessBuilder processBuilder = new ProcessBuilder(exePath);
